1a903e0dc88d700cfd5514b75c4672ddcf5db005
Aziz Alfauzi <azizalfauzi@student.telkomuniversity.ac.id>
2023-11-17 06:28:03 +0700

    Initial commit

---
+++ src/main/java/com/zuhaproject/spring/config/BelajarSpringConfigurationPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.zuhaproject.spring.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringConfigurationPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringConfigurationPropertiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zuhaproject/spring/config/BelajarSpringConfigurationPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuhaproject.spring.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringConfigurationPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
