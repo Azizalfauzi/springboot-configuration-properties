4584cd8826e1a739d77d69e460442e3000c261c1
Aziz Alfauzi <azizalfauzi@student.telkomuniversity.ac.id>
2023-11-17 15:26:41 +0700

    *SpringBoot - Application Properties

---
+++ src/test/java/com/zuhaproject/spring/config/appproperties/ApplicationPropertiesTest.java
@@ -0,0 +1,23 @@
+package com.zuhaproject.spring.config.appproperties;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+public class ApplicationPropertiesTest {
+
+    @Autowired
+    private Environment environment;
+
+    @Test
+    void testApplicationProperties() {
+        String applicationName = environment.getProperty("application.name");
+        Assertions.assertEquals("Belajar Spring Boot", applicationName);
+    }
+
+    @SpringBootApplication
+    public static class TestApplication {
+    }
+}
